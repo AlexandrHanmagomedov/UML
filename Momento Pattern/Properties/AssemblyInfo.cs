340d6b22eb6ace43cf2e2e652836316f39d1e283
Snejoke <98012287+AlexandrHanmagomedov@users.noreply.github.com>
2022-10-24 15:24:05 +0300

    Add files via upload

---
+++ Momento Pattern/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Momento Pattern")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Momento Pattern")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2abcc9b8-5225-4633-9892-1c3e0c114e58")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pattern factory пуля/Program.cs
@@ -0,0 +1,68 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Pattern_factory
+    {
+
+
+    abstract class Creator
+    {
+        public string Name { get; set; }
+
+        public Creator(string name )
+        {
+            this.Name = name;  
+        }
+        public abstract PuliaProduct FactoryMethod();
+    }
+ 
+    class MachineGunConcreteCreatorA : Creator
+    {
+        
+        public MachineGunConcreteCreatorA(string name) : base(name) { }
+        public override PuliaProduct FactoryMethod() {
+            Console.WriteLine($"{this.Name}");
+            return new MachineGunConcreteProductA(); }
+    }
+ 
+    class ShotcunConcreteCreatorB : Creator
+    {
+        public ShotcunConcreteCreatorB(string name) : base(name) { }
+        public override PuliaProduct FactoryMethod() {
+            Console.WriteLine($"{this.Name}");
+            return new ShotcunConcreteProductB(); }
+    }
+
+        abstract class PuliaProduct
+        { }
+
+        class MachineGunConcreteProductA : PuliaProduct
+        { 
+            public MachineGunConcreteProductA()
+        {
+            Console.WriteLine("Пуля для автомата готова");
+        }
+        }
+
+        class ShotcunConcreteProductB : PuliaProduct
+        { 
+            public ShotcunConcreteProductB()
+        {
+            Console.WriteLine("Картечь с солью готова");
+        }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            Creator creator = new MachineGunConcreteCreatorA("Автомат");
+            PuliaProduct pulia= creator.FactoryMethod();
+
+            creator = new ShotcunConcreteCreatorB("Дробовик");
+            PuliaProduct pulia1=creator.FactoryMethod();
+            }
+        }
+    }
