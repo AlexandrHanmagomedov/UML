baf719e5dc0c1904658ff5478be0e9a29e4602c0
Snejoke <98012287+AlexandrHanmagomedov@users.noreply.github.com>
2022-10-24 15:47:54 +0300

    Add files via upload

---
+++ Builder Auto/Auto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Builder_Auto
+{
+  
+    class Basement    {
+        public Basement(){Console.WriteLine("Basement ready");}
+    }
+    class Floor
+    {
+        public Floor() { Console.WriteLine("Floor ready"); }
+    }
+    class Roof
+    {
+        public Roof() { Console.WriteLine("Roof ready"); }
+    }
+    class House    {
+        ArrayList parts = new ArrayList();
+        public void Add(object part)        {
+            parts.Add(part);
+        }
+    }
+
+    
+
+    abstract class Builder    {
+        public abstract void BuildBasement();
+        public abstract void BuildStorey();
+        public abstract void BuildRoof();
+        public abstract House GetResult();
+    }
+
+    class ConcreteBuilder : Builder    {
+
+        House house = new House();
+        public override void BuildBasement(){house.Add(new Basement());}
+
+        public override void BuildStorey(){house.Add(new Floor());}
+
+        public override void BuildRoof(){house.Add(new Roof());}
+
+        public override House GetResult()        {
+            Console.WriteLine("House ready\n");
+            return house;
+        }
+    }
+
+}
---
+++ Builder Auto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Auto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConcreteBuilder builder = new ConcreteBuilder();
+
+            builder.BuildBasement();
+
+            builder.BuildStorey();
+
+            builder.BuildRoof();
+
+            builder.GetResult();
+            
+            
+        }
+    }
+}
